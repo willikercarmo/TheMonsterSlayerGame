ed6d1eb76605b335ce02d1e568447d4e3bde8f07
Williker Carmo <carmo.williker@gmail.com>
2022-02-24 10:17:24 +0000

    Adding a Special Attack

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -1,32 +1,40 @@
 function getRandomValue(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
   data() {
-      return {
-          playerHealth: 100,
-          monsterHealth: 100
-      }
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+    };
   },
   computed: {
-      monsterBarStyles() {
-          return {width: this.monsterHealth + '%'};
-      },
-      playerBarStyles() {
-          return {width: this.playerHealth + '%'}
-      }
+    monsterBarStyles() {
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + "%" };
+    },
   },
   methods: {
-      attackMonster() {
-        const attackValue = getRandomValue(5, 12);
-        this.monsterHealth -= attackValue;
-        this.attackPlayer();
-      },
-      attackPlayer() {
-        const attackValue = getRandomValue(8, 15);
-        this.playerHealth -= attackValue;
-      },
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
+    attackPlayer() {
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.attackPlayer();
+    },
   },
 });
 
