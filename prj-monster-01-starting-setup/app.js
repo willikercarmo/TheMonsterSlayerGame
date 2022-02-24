70cbb32f9fd030d9f4d526ef00bcc1287dc1c7d1
Williker Carmo <carmo.williker@gmail.com>
2022-02-24 09:25:12 +0000

    Project Setup & First Methods

---
+++ prj-monster-01-starting-setup/app.js
@@ -0,0 +1,24 @@
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+  data() {
+      return {
+          playerHealth: 100,
+          mosterHealth: 100,
+      }
+  },
+  methods: {
+      attackMonster() {
+        const attackValue = getRandomValue(5, 12);
+        this.mosterHealth -= attackValue;
+      },
+      attackPlayer() {
+        const attackValue = getRandomValue(8, 15);
+        this.attackPlayer -= attackValue;
+      },
+  },
+});
+
+app.mount("#game");
